c372991efdd2ad895d44d48e403ce874b1a2a3cd
Christian Jay F. Rosales <gargoth13@gmail.com>
2022-10-20 01:05:44 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dreams and Death - Foundry VTT System
+
+This game system by Sseventeen made for the campaign "Dreams and Death", but it can also be used in other settings.
+While initially based on the DnD5e system, it features a modular approach to character creation, as opposed to other TTRPG system's class-based approach. 
+
+This system is planned to contain all mechanics and data described in the ![system document](https://docs.google.com/document/d/1nhrAVppXUBosIOZ6HIEpDuepT2CnFjNIDQG6DrEwnrw/edit#heading=h.1jwkzt2mqljo).
+
+## npm scripts
+
+- `pack`
+
+`npm run pack`
+
+The pack script overwrites the `packs/` directory with new `.db` files made from the JSON files in `src/packs/<packName>/`
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const { series, parallel } = require("gulp");
+
+function cleanPacks(cb) {
+    fs.rmSync("packs", {
+        recursive: true,
+        force: true,
+    });
+    cb();
+}
+
+function convertPacks(cb) {
+    fs.mkdirSync("packs");
+    const packNames = fs.readdirSync("src/packs");
+    packNames.forEach((packName) => {
+        const packJsons = fs.readdirSync(`src/packs/${packName}`);
+        let dbData = "";
+        packJsons.forEach((packJson) => {
+            const data = JSON.stringify(JSON.parse(fs.readFileSync(`src/packs/${packName}/${packJson}`)));
+            dbData += data + "\n";
+        });
+        fs.writeFileSync(`packs/${packName}.db`, dbData);
+    });
+    cb();
+}
+
+exports.pack = series(cleanPacks, convertPacks);
