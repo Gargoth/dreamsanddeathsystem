98208da97437a7122da62931a48fea6d75f7e9da
Christian Jay F. Rosales <gargoth13@gmail.com>
2022-10-20 21:43:18 +0800

    fix: Removed gulp scripts related to packs

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,5 @@
-const fs = require("fs");
 const { series, parallel, src, dest, watch } = require("gulp");
 const less = require("gulp-less");
-
-function cleanPacks(cb) {
-    fs.rmSync("packs", {
-        recursive: true,
-        force: true,
-    });
-    cb();
-}
-
-function convertPacks(cb) {
-    fs.mkdirSync("packs");
-    const packNames = fs.readdirSync("src/packs");
-    packNames.forEach((packName) => {
-        const packJsons = fs.readdirSync(`src/packs/${packName}`);
-        let dbData = "";
-        packJsons.forEach((packJson) => {
-            const data = JSON.stringify(
-                JSON.parse(fs.readFileSync(`src/packs/${packName}/${packJson}`))
-            );
-            dbData += data + "\n";
-        });
-        fs.writeFileSync(`packs/${packName}.db`, dbData);
-    });
-    cb();
-}
-
-function watchPacks(cb) {
-    watch("src/packs/*/*.json", series(cleanPacks, convertPacks));
-    cb();
-}
 
 function transpileLess(cb) {
     src("src/style/dreamsanddeath.less").pipe(less()).pipe(dest("./styles/"));
@@ -43,5 +12,4 @@ function watchLess(cb) {
 }
 
 exports.less = series(transpileLess);
-exports.watch = parallel(watchPacks, watchLess);
-exports.pack = series(cleanPacks, convertPacks);
+exports.watch = parallel(watchLess);
